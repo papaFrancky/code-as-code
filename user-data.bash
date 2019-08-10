34b2c59213b13d8e0477ba561b36034a0257c006
papaFrancky <franck.levesquegmail.com>
2019-08-10 21:35:43 +0000

    reworked user-data script and playbook.

--- user-data.bash
+++ user-data.bash
@@ -3,19 +3,29 @@
 
 ####################  FUNCTIONS & VARS  ####################
 
+awsRegion=$(	curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/[a-z]$//' )
+
+instanceId=$(	curl -s http://169.254.169.254/latest/meta-data/instance-id )
+
+instanceName=$( aws ec2 describe-instances						\
+			--filters Name=instance-id,Values=${instanceId}			\
+			--query 'Reservations[].Instances[].Tags[?Key==`Name`].Value[]'	\
+			--region=${awsRegion}						\
+			--output text )
+
 # Ansible install
 installAnsible() {
-  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+  curl -s https://bootstrap.pypa.io/get-pip.py -o get-pip.py
   sudo python get-pip.py
   sudo pip install ansible boto3 paramiko cryptography
 }
 
 # Retrieve the ansible code from the S3 bucket
 getAnsibleCode() {
-  aws s3 sync s3://demo-infra-s3-bucket/bastion .
+  aws s3 sync s3://demo-infra-s3-bucket/${instanceName} . --region=${awsRegion}
 }
 
-# Run the ansible playbook
+# Run the ansible playbook 
 runAnsibleCode () {
   ansible-playbook -c local site.yml
 }
