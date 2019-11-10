24cb3b4f0ea311c348b114074545d817a3dfc4fd
franck.levesque@gmail.com <franck.levesque@gmail.com>
2019-11-10 17:24:26 +0100

    Changes in aws_cli.bash

--- aws_cli/aws_cli.bash
+++ aws_cli/aws_cli.bash
@@ -6,34 +6,37 @@ aws_access_key_id=
 aws_secret_access_key=
 export aws_access_key_id aws_secret_access_key
 
-VPC_Name="console"
+VPC_Name="demo"
 Region="eu-west-3"
-Name="awscli-bastion"
+AZ="demo-public-${Region}a"
+Instance_Name="awscli-bastion"
+Instance_Type="t2.micro"
+Key_Name="bastion"
 
 
 
 # INSTANCE PROFILE
 # ----------------
 
-Policy_List=" route53-upsert-records
-              s3-bastion-access
-              ec2-describe-instances
-              ec2-describe-tags"
+Policies_List=" route53-upsert-records
+                s3-bastion-access
+                ec2-describe-instances
+                ec2-describe-tags"
 
 
 # creation du role ec2 pour l'instance
-aws iam create-role     \
-  --region ${Region}    \
-  --role-name "${Name}" \
+aws iam create-role               \
+  --region ${Region}              \
+  --role-name "${Instance_Name}"  \
   --assume-role-policy-document file://files/policy_ec2_trust.json
 
 
 # definition des policies liees au role de l'instance
-for Policy in ${Policy_List}; do
-  aws iam put-role-policy     \
-    --region ${Region}        \
-    --role-name "${Name}"     \
-    --policy-name "${Policy}" \
+for Policy in ${Policies_List}; do
+  aws iam put-role-policy           \
+    --region ${Region}              \
+    --role-name "${Instance_Name}"  \
+    --policy-name "${Policy}"       \
     --policy-document file://files/${Policy}.json
 done
 
@@ -41,14 +44,14 @@ done
 # creation de l'instance-profile
   aws iam create-instance-profile \
     --region ${Region}            \
-    --instance-profile-name "${Name}" 
+    --instance-profile-name "${Instance_Name}" 
 
 
 # rattachement du role a l'instance-profile
-  aws iam add-role-to-instance-profile  \
-    --region ${Region}                  \
-    --instance-profile-name "${Name}"   \
-    --role-name "${Name}"
+  aws iam add-role-to-instance-profile          \
+    --region ${Region}                          \
+    --instance-profile-name "${Instance_Name}"  \
+    --role-name "${Instance_Name}"
 
 
 
@@ -64,20 +67,26 @@ VPC_Id=$( aws ec2 describe-vpcs                           \
 
 
 # creation du security-group
-aws ec2 create-security-group --region ${Region} --group-name ${Name} --description "${Name}" --vpc-id ${VPC_Id}
+aws ec2 create-security-group       \
+  --region ${Region}                \
+  --group-name ${Instance_Name}     \
+  --description "${Instance_Name}"  \
+  --vpc-id ${VPC_Id}
 
 
 # recuperation de l'id du security-group
-SG_Id=$(  aws ec2 describe-security-groups                                        \
-            --region=${Region}                                                    \
-            --filters Name=vpc-id,Values=${VPC_Id} Name=group-name,Values=${Name} \
-            --query 'SecurityGroups[].GroupId'                                    \
-            --output=text
- )
+SG_Id=$(  aws ec2 describe-security-groups                                                  \
+            --region ${Region}                                                              \
+            --filters Name=vpc-id,Values=${VPC_Id} Name=group-name,Values=${Instance_Name}  \
+            --query 'SecurityGroups[].GroupId'                                              \
+            --output=text )
 
 
 # Apposition du tag 'Name'
-aws ec2 create-tags --resources ${SG_Id} --tags Key=Name,Value=${Name}
+aws ec2 create-tags     \
+  --region ${Region}    \
+  --resources ${SG_Id}  \
+  --tags Key=Name,Value=${Instance_Name}
 
 
 # Creation des regles de firewall en entree
@@ -88,20 +97,45 @@ aws ec2 authorize-security-group-ingress --region ${Region} --group-id ${SG_Id} 
 
 
 # EC2 INSTANCE CREATION
-- name: definition de l'autoscaling launch configuration
-  ec2_lc:
-    region: "{{ region }}"
-    state: present
-    name: "lc-{{ Name }}"
-    image_id: "{{ amazon_linux }}"
-    key_name: "{{ Name }}"
-    instance_profile_name: "instance-profile-{{ Name }}"
-    security_groups: [ "{{ sg_id.stdout }}" ]
-    instance_type: "{{ instance_type }}"
-    assign_public_ip: no
-    #user_data: "{{ lookup('file', '../files/user_data') }}"
-    volumes:
-      - device_name: "/dev/xvda"
-        volume_size: 20
-        volume_type: gp2
-        delete_on_termination: true
+
+AMI_Id=$( aws ssm get-parameters                                                  \
+            --region ${Region}                                                    \
+            --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 \
+            --query 'Parameters[].Value'                                          \
+            --output text )
+
+
+Subnet_Id=$( aws ec2 describe-subnets               \
+            --region ${Region}                      \
+            --filters Name=tag:Name,Values="${AZ}"  \
+            --query Subnets[].SubnetId              \
+            --output text )
+
+aws ec2 run-instances                           \
+  --region ${Region}                            \
+  --count 1                                     \
+  --instance-type ${Instance_Type}              \
+  --image-id ${AMI_Id}                          \
+  --security-group-ids ${SG_Id}                 \
+  --key-name ${Key_Name}                        \
+  --subnet-id ${Subnet_Id}                      \
+  --associate-public-ip-address                 \
+  --iam-instance-profile Name=${Instance_Name}  \
+  --user-data file://files/user-data.bash       \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=cost-center,Value=cc123}]' 'ResourceType=volume,Tags=[{Key=cost-center,Value=cc123}]' 
+  #--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${Instance_Name}}]" 
+
+  # aws ec2 describe-instances --query Reservations[].Instances[].InstanceId --filters "Name=tag:Name,Values=awscli-bastion"
+
+Instance_Id=$(  aws ec2 describe-instances --query Reservations[].Instances[].InstanceId | \
+                sort | tail -1 | sed 's/["|,]//g' | awk '{print $1}' )
+
+# Apposition du tag 'Name'
+aws ec2 create-tags     \
+  --region ${Region}    \
+  --resources ${Instance_Id}  \
+  --tags Key=Name,Value=${Instance_Name}
+
+  
+
+
---
+++ aws_cli/files/user-data.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+####################  FUNCTIONS & VARS  ####################
+
+awsRegion=$(	curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/[a-z]$//' )
+
+instanceId=$(	curl -s http://169.254.169.254/latest/meta-data/instance-id )
+
+instanceName=$( aws ec2 describe-instances						\
+			--filters Name=instance-id,Values=${instanceId}			\
+			--query 'Reservations[].Instances[].Tags[?Key==`Name`].Value[]'	\
+			--region=${awsRegion}						\
+			--output text )
+
+# Ansible install
+installAnsible() {
+  curl -s https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+  sudo python get-pip.py
+  sudo pip install --upgrade pip
+  sudo pip install ansible boto  boto3 paramiko cryptography
+}
+
+# Retrieve the ansible code from the S3 bucket
+getAnsibleCode() {
+  mkdir /install
+  aws s3 sync s3://demo-infra-s3-bucket/${instanceName} /install/. --region=${awsRegion}
+}
+
+# Run the ansible playbook 
+runAnsibleCode () {
+  cd /install
+  ansible-playbook -c local site.yml
+}
+
+
+####################  CORE  ####################
+
+installAnsible
+getAnsibleCode
+runAnsibleCode
+
