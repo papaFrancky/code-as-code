f5b6b77c34c7b55843e8a3e620523ea48e092f0d
Franck Levesque <franck.levesque@gmail.com>
2019-12-04 02:12:03 +0100

    Corrections in user-data 'awscli and terraform)

--- aws_cli/files/user-data.bash
+++ aws_cli/files/user-data.bash
@@ -3,40 +3,42 @@
 
 ####################  FUNCTIONS & VARS  ####################
 
-awsRegion=$(	curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/[a-z]$//' )
+awsRegion=$(    curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/[a-z]$//' )
+instanceId=$(   curl -s http://169.254.169.254/latest/meta-data/instance-id )
 
-instanceId=$(	curl -s http://169.254.169.254/latest/meta-data/instance-id )
 
-instanceName=$( aws ec2 describe-instances						\
-			--filters Name=instance-id,Values=${instanceId}			\
-			--query 'Reservations[].Instances[].Tags[?Key==`Name`].Value[]'	\
-			--region=${awsRegion}						\
-			--output text )
-
-# Ansible install
-installAnsible() {
+# Tools installation
+installTools () {
   curl -s https://bootstrap.pypa.io/get-pip.py -o get-pip.py
   sudo python get-pip.py
   sudo pip install --upgrade pip
-  sudo pip install ansible boto  boto3 paramiko cryptography
+  sudo pip install awscli ansible boto boto3 paramiko cryptography
 }
 
+
 # Retrieve the ansible code from the S3 bucket
-getAnsibleCode() {
+getAnsibleCode () {
   mkdir /install
+  instanceName=$( aws ec2 describe-instances                                            \
+                        --filters Name=instance-id,Values=${instanceId}                 \
+                        --query 'Reservations[].Instances[].Tags[?Key==`Name`].Value[]' \
+                        --region=${awsRegion}                                           \
+                        --output text )
   aws s3 sync s3://demo-infra-s3-bucket/${instanceName} /install/. --region=${awsRegion}
 }
 
-# Run the ansible playbook 
+
+# Run the ansible playbook
 runAnsibleCode () {
   cd /install
   ansible-playbook -c local site.yml
 }
 
 
+
 ####################  CORE  ####################
 
-installAnsible
+installTools
 getAnsibleCode
 runAnsibleCode
 
